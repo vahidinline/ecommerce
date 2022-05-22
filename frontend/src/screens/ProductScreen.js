59d90e2e5aa54920a9581e607569263aadb3d7da
vahid Afshari <vahidinline@gmail.com>
2022-05-22 19:19:24 +0300

    add router

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  return <div>{slug}</div>;
+}
