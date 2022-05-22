9f681f0f177ca198cbf5c518bc2534fa8bf76d1f
vahid Afshari <vahidinline@gmail.com>
2022-05-22 17:38:04 +0300

    product-listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +6,27 @@ function App() {
       <header>
         <a href="/">e-commerce</a>
       </header>
-      <main>Lost Products</main>
+      <main>
+        <h1>Featured Productss</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
