8b2be42cb8ed9256620e2d8e479d34bacfc48dc4
vahid Afshari <vahidinline@gmail.com>
2022-05-22 16:52:56 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">e-commerce</a>
+      </header>
+      <main>Lost Products</main>
+    </div>
+  );
+}
+
+export default App;
