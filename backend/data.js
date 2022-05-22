49c770492427769a5b6faeee6484643ad7e834dc
vahid Afshari <vahidinline@gmail.com>
2022-05-23 00:47:59 +0300

    add backend

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Watch',
+      slug: 'watch-7',
+      category: 'accessories',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Apple watch 7 nike',
+    },
+    {
+      name: 'Watch',
+      slug: 'watch-6',
+      category: 'accessories',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Apple watch 6 nike',
+    },
+    {
+      name: 'Watch',
+      slug: 'watch-se',
+      category: 'accessories',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Apple watch SE nike',
+    },
+  ],
+};
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log('Server is running');
+});
