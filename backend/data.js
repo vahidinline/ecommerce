d24eb981693a3c1f767acbca2ebd9f8561cb43a2
vahid Afshari <vahidinline@gmail.com>
2022-05-23 16:12:13 +0300

    add bootstrap

--- backend/data.js
+++ backend/data.js
@@ -14,6 +14,18 @@ const data = {
     },
     {
       name: 'Watch',
+      slug: 'watch-7-nike',
+      category: 'accessories',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Apple watch 7',
+    },
+    {
+      name: 'Watch',
       slug: 'watch-6',
       category: 'accessories',
       image: '/images/p2.jpg',
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">e-commerce</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <LinkContainer to="/">
+                  <Navbar.Brand>e-commerce</Navbar.Brand>
+                </LinkContainer>
+              </Navbar.Brand>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">Footer</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
